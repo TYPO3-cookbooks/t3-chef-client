7636ba9b09774810a78ca26c6d4fab06e59fdda4
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-17 21:22:05 +0200

    [CLEANUP] Formatting of attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,16 @@
 # we run chef through cron every 30 minutes (with a splay of up to 10 minutes)
-default[:chef_client][:init_style] = "none"
-default[:chef_client][:cron][:hour] = "*"
-default[:chef_client][:cron][:minute] = "0,30"
-default[:chef_client][:cron][:log_file] = "/var/log/chef/client.log"
-default[:chef_client][:cron][:append_log] = true
-default[:chef_client][:splay] = "600"
+default['chef_client']['init_style'] = 'none'
+default['chef_client']['cron']['hour'] = '*'
+default['chef_client']['cron']['minute'] = '0,30'
+default['chef_client']['cron']['log_file'] = '/var/log/chef/client.log'
+default['chef_client']['cron']['append_log'] = true
+default['chef_client']['splay'] = '600'
 
 #<> avoid the restart of the chef-client service that the upstream cookbook initiates (see issue chef-client#284)
-default[:chef_client][:log_rotation][:postrotate] = nil
+default['chef_client']['log_rotation']['postrotate'] = nil
 
-default[:chef_client][:config][:ssl_verify_mode] = ":verify_peer"
-default[:chef_client][:config][:chef_server_url] = "https://chef.typo3.org"
+default['chef_client']['config']['ssl_verify_mode'] = ':verify_peer'
+default['chef_client']['config']['chef_server_url'] = 'https://chef.typo3.org'
 
-default[:omnibus_updater][:version] = "12.19.36"
-# default[:omnibus_updater][:direct_url] = "https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_11.12.0-1_amd64.deb"
-default[:omnibus_updater][:cache_dir] = File.join(Chef::Config[:file_cache_path], "chef-client")
+default['omnibus_updater']['version'] = '12.19.36'
+default['omnibus_updater']['cache_dir'] = File.join(Chef::Config[:file_cache_path], 'chef-client')
