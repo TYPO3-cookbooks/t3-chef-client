b660b75a9eed579e83b71d191971c7c33705734b
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-17 21:05:50 +0200

    [TASK] Switch to v2 pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,4 @@
+@Library('chefci') _
+org.typo3.chefci.v2.cookbook.CookbookPipeline.builder(this, steps)
+        .buildDefaultPipeline()
+        .execute()
--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for chef-client"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-
-version          "0.4.2"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends          "chef-client", "= 4.3.3"
 depends          "chef_handler"
